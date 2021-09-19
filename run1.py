e37a1cb0050b013933638091c4d420e1fdeb15e2
CamilleTrimbur <camille.m.trimbur>
2021-09-19 14:25:29 -0700

    adding run1 init

---
+++ run1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env micropython
+from ev3dev2.motor import *
+from ev3dev2.sensor import *
+from robot import *
+
+bot = Robot(OUTPUT_A, OUTPUT_B, 56, 15, INPUT_1, INPUT_2,
+            INPUT_4, INPUT_3, OUTPUT_C, OUTPUT_D)
+bot.gyro_sensor.reset()
+bot.gyro_straight(25, 60, 0.25)

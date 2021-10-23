fa1a07593436baf3ccb4db5f16fac6382af07b46
KiraDye <kira.dye@gmail.com>
2021-10-22 19:33:37 -0700

    Made sure Run 3 concept works

--- Run 3.py
+++ Run 3.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env micropython
 from ev3dev2.motor import *
 from ev3dev2.sensor import *
+from time import sleep
 
 from robot import *
 
 bot = Robot(OUTPUT_A, OUTPUT_B, 56, 15, INPUT_1, INPUT_2,
-            INPUT_4, motor1=LargeMotor(OUTPUT_C), motor2=LargeMotor(OUTPUT_D))
+            gyro_sensor_port=INPUT_3, motor1=LargeMotor(OUTPUT_C), motor2=LargeMotor(OUTPUT_D))
 
-bot.motor2.on_for_rotations(20, 0.1)
-bot.motor2.on_for_rotations(10, -0.2)
+bot.gyro_sensor.reset()
+time.sleep(0.05)
+bot.motor2.on_for_rotations(20,-0.1)
+bot.gyro_straight(40,22,0.5)
+bot.gyro_turn(39, 40, 26)
